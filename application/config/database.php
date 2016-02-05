8ddd98b8bde592632c367956e8bf49b075956705
KumHoe Lau <nerojz86@gmail.com>
2016-02-05 17:47:01 +0800

    Update configuration

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'mysql';
-$db['default']['database'] = 'ci_doctrine';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
